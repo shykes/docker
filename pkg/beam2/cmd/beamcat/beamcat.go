6e09fdeab9c239a9b415698ec857ed2c819719c2
Solomon Hykes <solomon@dotcloud.com>
2014-01-03 18:11:18 -0800

    beam2: implement a toy job engine on a unix transport

--- pkg/beam2/cmd/beamcat/beamcat.go
+++ pkg/beam2/cmd/beamcat/beamcat.go
@@ -90,8 +90,7 @@ func handleRequests(t *unix.Transport, dst io.Writer) {
 			go copyLines(os.Stdout, st, fmt.Sprintf("[%d/%d] ", st.Parent().Id(), st.Id()))
 			continue
 		}
-		job := st
-		scanner := bufio.NewScanner(job)
+		scanner := bufio.NewScanner(st)
 		scanner.Scan()
 		if err := scanner.Err(); err != nil {
 			log.Fatal("read from peer: %s", err)
@@ -99,45 +98,34 @@ func handleRequests(t *unix.Transport, dst io.Writer) {
 		fmt.Printf("---> %s\n", scanner.Text())
 		wg.Add(1)
 		go func() {
-			words := strings.Split(scanner.Text(), " ")
-			stdout := t.New(job)
-			if err := stdout.Send(); err != nil {
+			words := strings.Split(strings.Trim(scanner.Text(), " \t"), " ")
+			job := &Job{
+				Stream: st,
+				Name: words[0],
+				Args: words[1:],
+			}
+			job.Printf("job-name=%s\n", job.Name)
+			job.Printf("job-args=%s\n", strings.Join(job.Args, "\x00"))
+			job.Stdout = job.New()
+			if err := job.Stdout.Send(); err != nil {
 				log.Fatalf("send stdout: %s", err)
 			}
-			stderr := t.New(job)
-			if err := stderr.Send(); err != nil {
+			job.Stderr = job.New()
+			if err := job.Stderr.Send(); err != nil {
 				log.Fatalf("send stderr: %s", err)
 			}
-			if words[0] == "download" {
-				if len(words) < 2 {
-					stderr.Printf("Error: please specify a url\n")
-					job.Printf("status=1\n")
-				} else {
-					stderr.Printf("Downloading from %s\n", words[1])
-					resp, err := http.Get(words[1])
-					if err != nil {
-						stderr.Printf("get: %s\n", err)
-						job.Printf("status=2\n")
-					} else {
-						fmt.Fprintf(stderr, "%s\n", resp.Status)
-						io.Copy(stdout, resp.Body)
-						job.Printf("status=0\n")
-					}
-				}
-			} else {
-				cmd := exec.Command("sh", "-c", scanner.Text())
-				cmd.Stdout = stdout
-				cmd.Stderr = stderr
-				if err := cmd.Run(); err != nil {
-					stderr.Printf("error: %s\n", err)
-					job.Printf("status=127\n")
-				} else {
-					fmt.Fprintf(st, "status=%s\n", cmd.ProcessState)
+			switch job.Name {
+				case "download": jobDownload(job)
+				case "listen":   jobListen(job)
+				case "exec":     jobExec(job)
+				default: {
+					job.Stderr.Printf("No such command: %s\n", job.Name)
+					job.Printf("status=2\n")
 				}
 			}
-			st.Close()
-			stdout.Close()
-			stderr.Close()
+			job.Stdout.Close()
+			job.Stderr.Close()
+			job.Close()
 		}()
 	}
 }
@@ -178,4 +166,102 @@ func connectGram(filename string) (conn *net.UnixConn, server bool, err error) {
 		return conn, true, nil
 	}
 	return conn, false, nil
+}
+
+func jobListen(job *Job) {
+	if len(job.Args) < 2 {
+		job.Stderr.Printf("Usage: %s PROTO ADDRESS\n", job.Name)
+		job.Printf("status=1\n")
+		return
+	}
+	proto := job.Args[0]
+	addr := job.Args[1]
+	job.Stderr.Printf("Listening on %s/%s", proto, addr)
+	l, err := net.Listen(proto, addr)
+	if err != nil {
+		job.Stderr.Printf("listen: %s\n", err)
+		job.Printf("status=1\n")
+		return
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			job.Stderr.Printf("accept: %s\n", err)
+			job.Printf("status=1\n")
+			return
+		}
+		job.Stderr.Printf("New connection from %s\n", conn.RemoteAddr())
+		st := job.New()
+		if err := st.Send(); err != nil {
+			job.Stderr.Printf("send: %s\n", err)
+			job.Printf("status=1\n")
+			return
+		}
+		go func() {
+			Splice(st, conn)
+			conn.Close()
+			st.Close()
+		}()
+	}
+}
+
+func Splice(a, b io.ReadWriter) (err error) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		_, e := io.Copy(a, b)
+		if e != nil && err == nil {
+			err = e
+		}
+		wg.Done()
+	}()
+	go func() {
+		_, e := io.Copy(b, a)
+		if e != nil && err == nil {
+			err = e
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	return
+}
+
+func jobExec(job *Job) {
+	cmd := exec.Command("sh", "-c", strings.Join(job.Args, " "))
+	cmd.Stdout = job.Stdout
+	cmd.Stderr = job.Stderr
+	if err := cmd.Run(); err != nil {
+		job.Stderr.Printf("error: %s\n", err)
+		job.Printf("status=127\n")
+		return
+	}
+	job.Printf("status=%s\n", cmd.ProcessState)
+}
+
+type Job struct {
+	*unix.Stream
+	Name string
+	Args []string
+	Stdout *unix.Stream
+	Stderr *unix.Stream
+}
+
+
+func jobDownload(job *Job) {
+	if len(job.Args) < 1 {
+		job.Stderr.Printf("Usage: %s URL\n", job.Name)
+		job.Printf("status=1\n")
+		return
+	}
+	url := job.Args[0]
+	job.Stderr.Printf("Downloading from %s\n", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		job.Stderr.Printf("GET %s: %s\n", url, err)
+		job.Printf("status=1\n")
+		return
+	}
+	job.Stderr.Printf("%s\n", resp.Status)
+	io.Copy(job.Stdout, resp.Body)
+	job.Printf("status=0\n")
 }
