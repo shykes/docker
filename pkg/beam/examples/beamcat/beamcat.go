eacb0c601a604091ab3625b63e0d9b400f7d6a4e
Solomon Hykes <solomon@dotcloud.com>
2014-01-05 18:11:01 -0800

    beam: move Route and Server out of the examples and into beam

--- pkg/beam/examples/beamcat/beamcat.go
+++ pkg/beam/examples/beamcat/beamcat.go
@@ -22,7 +22,7 @@ func main() {
 	}
 	session := beam.New(sock, server)
 	defer session.Close()
-	srv := NewServer(session)
+	srv := beam.NewServer(session)
 	newJobs := srv.NewRoute()
 	newJobs.Parent()
 	newJobs.Headers("content-type", "beam-job")
@@ -88,7 +88,7 @@ func handleNewJob(st *beam.Stream) {
 	job.Close()
 }
 
-func handleUserInput(src io.Reader, srv *Server) {
+func handleUserInput(src io.Reader, srv *beam.Server) {
 	defer fmt.Printf("handleUserInput done\n")
 	var wg sync.WaitGroup
 	defer wg.Wait()
@@ -130,116 +130,6 @@ func handleUserInput(src io.Reader, srv *Server) {
 }
 
 // Server
-
-type Server struct {
-	session *beam.Session
-	routes  []*Route
-}
-
-func (srv *Server) Session() *beam.Session {
-	return srv.session
-}
-
-func NewServer(session *beam.Session) *Server {
-	return &Server{session: session}
-}
-
-func (srv *Server) NewRoute() *Route {
-	route := &Route{}
-	srv.routes = append(srv.routes, route)
-	return route
-}
-
-type Route struct {
-	filters []func(*beam.Stream) bool
-	fn      func(*beam.Stream)
-}
-
-func (r *Route) Parent(parentIds ...int) *Route {
-	r.filters = append(r.filters, func(st *beam.Stream) (match bool) {
-		parent := st.Parent()
-		if parent == nil {
-			return len(parentIds) == 0
-		}
-		for _, parentId := range parentIds {
-			if parent.Id() == parentId {
-				return true
-			}
-		}
-		return false
-	})
-	return r
-}
-
-func (r *Route) Headers(pairs ...string) *Route {
-	r.filters = append(r.filters, func(st *beam.Stream) (match bool) {
-		for i := 0; i < len(pairs); i += 2 {
-			key := pairs[i]
-			var value string
-			if len(pairs) > i+1 {
-				value = pairs[i+1]
-			}
-			if value == "" {
-				if !st.Metadata.Exists(key) {
-					return false
-				}
-				continue
-			}
-			if st.Metadata.Get(key) != value {
-				return false
-			}
-		}
-		return true
-	})
-	return r
-}
-
-func (r *Route) HandleFunc(fn func(*beam.Stream)) *Route {
-	r.fn = fn
-	return r
-}
-
-func (r *Route) Match(st *beam.Stream) (match bool) {
-	for _, filter := range r.filters {
-		if filter(st) == false {
-			return false
-		}
-	}
-	return true
-}
-
-func (r *Route) Handle(st *beam.Stream) {
-	if r.fn == nil {
-		return
-	}
-	r.fn(st)
-}
-
-func (srv *Server) Serve() error {
-	var wg sync.WaitGroup
-	defer wg.Wait()
-	for {
-		st, err := srv.session.Receive()
-		if err != nil {
-			return fmt.Errorf("receive: %s", err)
-		}
-		fmt.Printf("+++ %d %s\n", st.Id(), st.Metadata.ShortString())
-		for i := range srv.routes {
-			// Last route added wins
-			route := srv.routes[len(srv.routes)-i-1]
-			if route.Match(st) {
-				wg.Add(1)
-				go func() {
-					route.Handle(st)
-					st.Close()
-					wg.Done()
-				}()
-				continue
-			}
-		}
-		fmt.Printf("No matching route for inbound stream %d. Dropping\n", st.Id())
-	}
-}
 
 func connectStream(filename string) (conn *net.UnixConn, server bool, err error) {
 	addr, err := net.ResolveUnixAddr("unix", filename)
---
+++ pkg/beam/route.go
@@ -0,0 +1,67 @@
+package beam
+
+type Route struct {
+	filters []func(*Stream) bool
+	fn      func(*Stream)
+}
+
+func (r *Route) Parent(parentIds ...int) *Route {
+	r.filters = append(r.filters, func(st *Stream) (match bool) {
+		parent := st.Parent()
+		if parent == nil {
+			return len(parentIds) == 0
+		}
+		for _, parentId := range parentIds {
+			if parent.Id() == parentId {
+				return true
+			}
+		}
+		return false
+	})
+	return r
+}
+
+func (r *Route) Headers(pairs ...string) *Route {
+	r.filters = append(r.filters, func(st *Stream) (match bool) {
+		for i := 0; i < len(pairs); i += 2 {
+			key := pairs[i]
+			var value string
+			if len(pairs) > i+1 {
+				value = pairs[i+1]
+			}
+			if value == "" {
+				if !st.Metadata.Exists(key) {
+					return false
+				}
+				continue
+			}
+			if st.Metadata.Get(key) != value {
+				return false
+			}
+		}
+		return true
+	})
+	return r
+}
+
+func (r *Route) HandleFunc(fn func(*Stream)) *Route {
+	r.fn = fn
+	return r
+}
+
+func (r *Route) Match(st *Stream) (match bool) {
+	for _, filter := range r.filters {
+		if filter(st) == false {
+			return false
+		}
+	}
+	return true
+}
+
+func (r *Route) Handle(st *Stream) {
+	if r.fn == nil {
+		return
+	}
+	r.fn(st)
+}
+
