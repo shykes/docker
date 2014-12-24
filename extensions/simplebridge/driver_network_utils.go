77480056839d086a4c6dd15c24fb11c0e54f0515
Erik Hollensbe <github@hollensbe.org>
2014-12-24 11:42:01 +0000

    better errors for (*BridgeDriver).createBridge
    Docker-DCO-1.1-Signed-off-by: Erik Hollensbe <github@hollensbe.org> (github: erikh)

--- extensions/simplebridge/driver_network_utils.go
+++ extensions/simplebridge/driver_network_utils.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net"
 
 	"github.com/docker/docker/pkg/iptables"
@@ -13,12 +12,15 @@ import (
 	"github.com/vishvananda/netlink/nl"
 )
 
+func bridgeError(typ string, err error) error {
+	return fmt.Errorf("createBridge: %s: %v", typ, err)
+}
+
 func (d *BridgeDriver) createBridge(id string, vlanid uint, port uint, peer, device string) (*BridgeNetwork, error) {
 	dockerbridge := &netlink.Bridge{netlink.LinkAttrs{Name: id}}
 
 	linkval, err := d.getInterface(id, dockerbridge)
 	if err != nil {
-		log.Println("Error get interface", err)
 		return nil, err
 	}
 	dockerbridge = linkval.(*netlink.Bridge)
@@ -30,7 +32,7 @@ func (d *BridgeDriver) createBridge(id string, vlanid uint, port uint, peer, dev
 
 	addrList, err := netlink.AddrList(dockerbridge, nl.GetIPFamily(addr.IP))
 	if err != nil {
-		return nil, err
+		return nil, bridgeError("list addresses for bridge", err)
 	}
 
 	var found bool
@@ -42,18 +44,16 @@ func (d *BridgeDriver) createBridge(id string, vlanid uint, port uint, peer, dev
 	}
 	if !found {
 		if err := netlink.AddrAdd(dockerbridge, &netlink.Addr{IPNet: addr}); err != nil {
-			log.Println("Error add addr", err)
-			return nil, err
+			return nil, bridgeError("bridge add address", err)
 		}
 	}
 
 	if err := netlink.LinkSetUp(dockerbridge); err != nil {
-		log.Println("Error up bridge", err)
-		return nil, err
+		return nil, bridgeError("bridge interface up", err)
 	}
 
 	if err := setupIPTables(id, addr); err != nil {
-		return nil, err
+		return nil, bridgeError("configure iptables", err)
 	}
 
 	var vxlan *netlink.Vxlan
@@ -61,8 +61,7 @@ func (d *BridgeDriver) createBridge(id string, vlanid uint, port uint, peer, dev
 	if peer != "" && device != "" {
 		iface, err := net.InterfaceByName(device)
 		if err != nil {
-			log.Println("Error get interface", err)
-			return nil, err
+			return nil, bridgeError("retrieve interface name", err)
 		}
 
 		vxlan = &netlink.Vxlan{
@@ -76,24 +75,21 @@ func (d *BridgeDriver) createBridge(id string, vlanid uint, port uint, peer, dev
 
 		linkval, err = d.getInterface(vxlan.LinkAttrs.Name, vxlan)
 		if err != nil {
-			log.Println("Error get interface", err)
-			return nil, err
+			return nil, bridgeError("retrieve interface name", err)
 		}
 		vxlan = linkval.(*netlink.Vxlan)
 
 		// ignore errors in case it was already set
 		if err := netlink.LinkSetMaster(vxlan, dockerbridge); err != nil {
-			log.Println("Error linksetmaster", err)
-			return nil, err
+			return nil, bridgeError("add vxlan interface to bridge", err)
 		}
 		if err := netlink.LinkSetUp(vxlan); err != nil {
-			log.Println("Error linksetmaster", err)
-			return nil, err
+			return nil, bridgeError("vxlan interface up", err)
 		}
 	}
 
 	if err := MakeChain(id, dockerbridge.LinkAttrs.Name); err != nil {
-		return nil, err
+		return nil, bridgeError("bootstrap iptables chain for bridge", err)
 	}
 
 	return &BridgeNetwork{
@@ -129,7 +125,7 @@ func (d *BridgeDriver) getInterface(prefix string, linkParams netlink.Link) (net
 	for i := 0; i < maxVethSuffix; i++ {
 		ethName = fmt.Sprintf("%s%d", prefix, i)
 		if len(ethName) > maxVethName+maxVethSuffixLen {
-			return nil, fmt.Errorf("EthName %q is longer than %d bytes", prefix, maxVethName)
+			return nil, fmt.Errorf("getInterface: EthName %q is longer than %d bytes", prefix, maxVethName)
 		}
 		if _, err := netlink.LinkByName(ethName); err != nil {
 			available = true
@@ -138,7 +134,7 @@ func (d *BridgeDriver) getInterface(prefix string, linkParams netlink.Link) (net
 	}
 
 	if !available {
-		return nil, fmt.Errorf("Cannot allocate more than %d ethernet devices for prefix %q", maxVethSuffix, prefix)
+		return nil, fmt.Errorf("getInterface: Cannot allocate more than %d ethernet devices for prefix %q", maxVethSuffix, prefix)
 	}
 
 	linkParams.Attrs().Name = ethName
